41f3a479daa06b8d71e16016aff3afd9a2ebb553
Dfreem <freemand@my.lanecc.edu>
2024-01-06 15:35:36 -0800

    tooltips, fix user uploads

--- Portfolio/Services/WebFileHandler.cs
+++ Portfolio/Services/WebFileHandler.cs
@@ -5,21 +5,15 @@ namespace Portfolio.Services;
 
 public class WebFileHandler : IDisposable
 {
-    public string? StoredFileName { get; set; }
-    public string? FileName { get; set; }
-    public IFormFile? HtmlFile { get; set; }
     public IBrowserFile? BrowserHtml { get; set; }
+    public string? HtmlFileContent { get; set; }
     public IBrowserFile? BrowserCss { get; set; }
-    public IFormFile? CssFile { get; set; }
-
+    public string? CssContent { get; set; }
     public void Dispose()
     {
 
-        StoredFileName ??= null;
-        FileName ??= null;
         BrowserHtml ??= null;
         BrowserCss ??= null;
-        CssFile ??= null;
         GC.SuppressFinalize(this);
 
     }
---
+++ READEME.md
@@ -0,0 +1,16 @@
+# Devin's Portfolio
+
+## TODO
+
+- [ ] Mobile Friendly
+- [ ] Tooltips
+- [ ] About
+  - [ ] the site
+  - [ ] me
+  - [ ] my resume
+- [ ] WebAssembly games
+- [ ] message text formatting/style
+- [ ] intro/docs/explanation of Transform Tool
+- [ ] Title for Discord widget (Let's Code)
+- [ ] new discord widget using json api
+- [ ] 
